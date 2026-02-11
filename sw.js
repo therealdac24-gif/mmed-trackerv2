d7e013a12f17f730d3db65e5837fadfc2fe68c90
therealdac24-gif <therealdac24@gmail.com>
2026-02-12 05:35:53 +1000

    Add files via upload
    update

--- sw.js
+++ sw.js
@@ -8,7 +8,13 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME).then((cache) => {
+            // Try to cache all, but don't fail properly if one fails (best effort for icon)
+            return cache.addAll(ASSETS).catch(err => {
+                console.log('SW: Cache addAll failed, trying essential assets', err);
+                return cache.addAll(['./', './index.html', './manifest.json']);
+            });
+        })
     );
 });
 
